066c6194c56d20ae83b20a362e50a570d49e31d7
Blibox <95865510+Blibox@users.noreply.github.com>
2023-02-02 19:01:30 +0100

    ouverture du xlsx fonctionnel

--- python/scripttest.py
+++ python/scripttest.py
@@ -1,7 +1,8 @@
 from variables import Keywords
+import pymongo as pm
 from openpyxl import load_workbook, Workbook
 import os
-import sys
+from urllib.parse import quote_plus
 
 
 # file = pd.read_excel(
@@ -18,8 +19,29 @@ def iterating_over_values(path, sheet_name):
     for value in sheet.iter_rows(
             min_col=0, max_col=11, min_row=1, max_row=402,
             values_only=False):
-        print(value[0].value)
+        l = []
+        for v in value:
+            l.append(v.value)
+        print(l)
 
 
-iterating_over_values(
-    './others/20200601_IRIT_clinicalTrials+publications.xlsx', Keywords.WS_CLINICALTRIALS_OBS)
+def connection():
+    username = quote_plus('<Jules>')
+    password = quote_plus('<C6IyTitjpiYeq4t4>')
+    cluster = '<SAE>'
+    uri = 'mongodb+srv://' + username + ':' + password + '@' + cluster + \
+        '.x1hujrr.mongodb.net'
+    client = pm.MongoClient(uri)
+    return client.test
+
+
+def __main__():
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(
+        file_dir, 'excels', '20200601_IRIT_clinicalTrials+publications.xlsx')
+    iterating_over_values(
+        file_path, Keywords.WS_CLINICALTRIALS_OBS.value)
+    # print(db)
+
+
+__main__()
